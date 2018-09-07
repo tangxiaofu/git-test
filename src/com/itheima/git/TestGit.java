a383d6f9ee5684611e8e7949cc330ce08c7157c3
zhangsan <zhangsan@itcast.com>
2018-09-07 20:08:40 +0800

    添加到git仓库

---
+++ src/com/itheima/git/TestGit.java
@@ -0,0 +1,10 @@
+package com.itheima.git;
+
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        System.out.println("hello word");
+        System.out.println("hello word");
+    }
+}
